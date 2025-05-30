a442ae83e17b892f0d39e9105e21bab5885ac063
Codwerk <36073831+CodeurCopieur@users.noreply.github.com>
2025-05-30 22:03:26 +0200

    Mise en place du dossier et des extensions

---
+++ 2.Créer-une-app-React/3.Les-bases-de-React/src/App.jsx
@@ -0,0 +1,12 @@
+import Container from "./lessons/1.Le-JSX/Container";
+
+function App() {
+
+  return (
+    <div className="main-content">
+      <Container />
+    </div>
+  )
+}
+
+export default App
---
+++ 2.Créer-une-app-React/3.Les-bases-de-React/src/lessons/1.Le-JSX/Container.jsx
@@ -0,0 +1,10 @@
+function Container() {
+    return (
+        <div>
+            <h1>Hello</h1>
+            <p>This is a container</p>
+        </div>
+    )
+}
+
+export default Container;
