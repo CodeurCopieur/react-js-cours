50a621b44fcb7392015f46a5b19aed5668812394
Codwerk <36073831+CodeurCopieur@users.noreply.github.com>
2025-06-01 22:51:37 +0200

    La syntaxe et les fonctionnalités JSX

--- 2.Créer-une-app-React/3.Les-bases-de-React/src/lessons/1.Le-JSX/Container.jsx
+++ 2.Créer-une-app-React/3.Les-bases-de-React/src/lessons/1.Le-JSX/Container.jsx
@@ -1,8 +1,30 @@
+import React, { useState } from 'react';
+
 function Container() {
+    const [name, setName] = useState("Widwerk");
+    const age = 20;
+    const handleClick = (e) => {
+        console.log("handleClick", e);
+        alert("handleClick", e);
+    }
     return (
         <div>
-            <h1>Hello</h1>
+            <h1>Hello {name}</h1>
             <p>This is a container</p>
+            <p>My name is {name.toUpperCase()}</p>
+            <p>My age is {age}</p>
+            <p>My name is {name.toUpperCase()} and my age is {age}</p>
+            <p className="text-red-500">
+                {name === "Widwerk" && <span>I'm John</span>}
+            </p>
+            <input 
+                type="text" 
+                placeholder="Enter your name" 
+                value={name} 
+                onChange={(e) => setName(e.target.value)}
+            />
+            <button onClick={() => alert("alert")}>onClick alert</button>
+            <button onClick={handleClick}>onClick ChandleClick</button>
         </div>
     )
 }
