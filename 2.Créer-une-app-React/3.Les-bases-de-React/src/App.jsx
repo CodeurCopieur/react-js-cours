460467b3fa3bdf81cea0089ce3fc89c9f099eec9
Codwerk <36073831+CodeurCopieur@users.noreply.github.com>
2025-06-04 00:11:31 +0200

    Mon premier composant

--- 2.Créer-une-app-React/3.Les-bases-de-React/src/App.jsx
+++ 2.Créer-une-app-React/3.Les-bases-de-React/src/App.jsx
@@ -1,10 +1,12 @@
 import Container from "./lessons/1.Le-JSX/Container";
+import Composant from "./lessons/2.Premier-composant/Container";
 
 function App() {
 
   return (
     <div className="main-content">
       <Container />
+      <Composant />
     </div>
   )
 }
---
+++ 2.Créer-une-app-React/3.Les-bases-de-React/src/lessons/2.Premier-composant/ListItem.jsx
@@ -0,0 +1,5 @@
+export default function ListItem() {
+  return (
+    <li>ListItem - 1</li>
+  )
+}
