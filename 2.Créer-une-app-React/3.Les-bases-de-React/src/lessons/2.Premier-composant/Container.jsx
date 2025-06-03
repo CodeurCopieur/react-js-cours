804137d0f4b4c2d2145a8ee6c0bd0cdd3084d921
Codwerk <36073831+CodeurCopieur@users.noreply.github.com>
2025-06-04 00:13:21 +0200

    Notre premier composant

---
+++ 2.Créer-une-app-React/3.Les-bases-de-React/src/lessons/2.Premier-composant/Container.jsx
@@ -0,0 +1,12 @@
+import ListItem from "./ListItem";
+
+export default function Container() {
+  return (
+    <div>
+      <h1>Premier Composant</h1>
+      <ul>
+        <ListItem />
+      </ul>
+    </div>
+  )
+}
