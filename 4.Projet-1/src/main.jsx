ac7252605e3de6036f6232e18d6e784c31680ae5
Codwerk <36073831+CodeurCopieur@users.noreply.github.com>
2025-06-22 12:41:28 +0200

    Explication de React.strictMode

--- 4.Projet-1/src/main.jsx
+++ 4.Projet-1/src/main.jsx
@@ -2,6 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+/* 
+StrictMode est un composant qui permet:
+Détecter les effets de bord non intentionnels
+Votre composant App sera rendu deux fois en développement
+Tous les useEffect s'exécuteront deux fois
+Les constructeurs de classe seront appelés deux fois
+Cela aide à détecter les effets de bord non intentionnels */
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
