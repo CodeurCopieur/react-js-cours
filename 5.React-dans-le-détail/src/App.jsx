9f18a3f41af3ed5ea9df341c447f7ad0fa7f3fa1
Codwerk <36073831+CodeurCopieur@users.noreply.github.com>
2025-07-16 21:43:08 +0200

    Les Fragments

---
+++ 5.React-dans-le-détail/src/App.jsx
@@ -0,0 +1,12 @@
+import Fragments from './lessons/1.Les-fragments/Container'
+
+function App() {
+
+  return (
+    <div className="main-content">
+      <Fragments/>
+    </div>
+  )
+}
+
+export default App
---
+++ 5.React-dans-le-détail/src/lessons/1.Les-fragments/Container.jsx
@@ -0,0 +1,11 @@
+export default function Container() {
+  return (
+    <>
+      <h1>Les fragments</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+    </>
+  )
+}
