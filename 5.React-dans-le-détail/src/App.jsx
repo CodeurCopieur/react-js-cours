51262e46d81936e0810f0ae919c0c0141947379f
Codwerk <louisws@outlook.fr>
2025-08-20 17:50:32 +0200

    La fonction de nettoyage

--- 5.React-dans-le-détail/src/App.jsx
+++ 5.React-dans-le-détail/src/App.jsx
@@ -1,10 +1,20 @@
+
 import Fragments from './lessons/1.Les-fragments/Container'
+import Hooks from './lessons/2.La-règle-des-hooks/Container'
+import UseEffect from './lessons/3.Le-hook-use-effect/Container'
+import CleanupFunction from './lessons/4.Cleanup-function/Container'
 
 function App() {
 
   return (
     <div className="main-content">
       <Fragments/>
+      <hr />
+      <Hooks/>
+      <hr />
+      <UseEffect/>
+      <hr />
+      <CleanupFunction/>
     </div>
   )
 }
