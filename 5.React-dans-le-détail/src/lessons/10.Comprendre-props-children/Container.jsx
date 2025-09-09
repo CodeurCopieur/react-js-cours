1a014dba97c2d6fbb7353c83e6525e20ce3ede99
Codwerk <louisws@outlook.fr>
2025-09-09 12:59:45 +0200

    Comprendre les "props.children"

---
+++ 5.React-dans-le-détail/src/lessons/10.Comprendre-props-children/Container.jsx
@@ -0,0 +1,12 @@
+import Child from './Child'
+export default function Container() {
+  return (
+    <div>
+      <h1>Props children</h1>
+      <Child>
+        <h1>Hello children</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, dolor sed incidunt fugiat odit sapiente aliquid repellendus quo. Quia ipsam officia laboriosam earum? Et incidunt, cumque nostrum dolores sapiente sed iusto nisi beatae nulla error! Vero provident vel architecto unde consequuntur non recusandae soluta minima fugiat maiores, illum, quas necessitatibus aliquid voluptate reprehenderit id sapiente sint ducimus! Iusto nisi explicabo quisquam necessitatibus, obcaecati consectetur repudiandae sit ullam cupiditate ipsa dicta ea laborum saepe odio voluptates perferendis amet exercitationem hic, rerum debitis! Fugiat consequatur harum perferendis. Reiciendis laudantium culpa hic itaque numquam temporibus provident eum pariatur corrupti aliquid incidunt libero nihil, ipsum veritatis ipsa mollitia sequi sunt veniam beatae voluptates necessitatibus. Vero harum aperiam perferendis assumenda eaque, blanditiis dicta, earum molestiae id ipsa doloribus autem tenetur? Aut ipsa quis possimus illo quo quasi dolore reprehenderit accusantium omnis quas blanditiis debitis animi ducimus, nulla natus aperiam eligendi fugiat repellendus vitae. Quae omnis incidunt, assumenda voluptatem ipsa, magni consectetur neque officiis impedit autem amet blanditiis, ad earum modi perferendis ea eum veniam accusamus voluptas natus numquam optio possimus ut expedita! Voluptate quasi nemo soluta officiis sunt? Aut delectus sit cum dolorum! Dolorem dolore dolorum ducimus sequi impedit fugiat quis est debitis placeat eius.</p>
+      </Child>
+    </div>
+  )
+}
