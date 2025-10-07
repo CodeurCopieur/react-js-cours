2816f3de102ec33b15abf17fff12e6b46cb1f067
Codwerk <louisws@outlook.fr>
2025-10-07 17:19:44 +0200

    Créer un système d'onglets

--- 6.Créer-des-composants-courants/src/App.jsx
+++ 6.Créer-des-composants-courants/src/App.jsx
@@ -1,10 +1,25 @@
 import ModalButton from './lessons/1.Créer-une-fenêtre-modale-avec-un-portal/ModalButton'
-
+import Tabs from './lessons/2.Coder-des-onglets/Tabs'
 function App() {
 
   return (
     <div className="min-h-screen bg-slate-700 pt-10">
+      
+      <hr />
+      <br />
+      <hr />
+      <br /><h1 style={{color: 'white'}}>Fênetres Modales</h1>
+      <hr />
+      <br />
       <ModalButton />
+      <hr />
+      <br />
+      <hr />
+      <br />
+      <h1 style={{color: 'white'}}>Coder des onglets</h1>
+      <hr />
+      <br />
+      <Tabs />
     </div>
   )
 }
--- 6.Créer-des-composants-courants/src/lessons/2.Coder-des-onglets/Tabs.jsx
+++ 6.Créer-des-composants-courants/src/lessons/2.Coder-des-onglets/Tabs.jsx
@@ -8,4 +8,31 @@ const tabsData = [
   {
     txt: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi illo a quibusdam voluptatem praesentium repellendus ab reiciendis iusto! Quod hic aliquam, voluptates expedita iure eius corrupti incidunt quos unde doloremque earum, eligendi sequi, nam praesentium. Eius voluptates ullam est omnis similique tempora enim. Aut necessitatibus nesciunt et voluptatibus excepturi. Magni totam modi, similique atque recusandae blanditiis ut necessitatibus explicabo qui?",
   },
-]
+];
+
+import { useState } from 'react';
+
+export default function Tabs() {
+  const [selectedTab, setSelectedTab] = useState(0);
+
+  return (
+    <div className='max-w-xl min-h-[300px] mx-auto rounded-md bg-slate-800 text-white p-4'>
+      <div className='flex divide-x divide-slate-700'>
+        {tabsData.map((tab, index) => (
+          <button 
+            key={index} 
+            disabled={selectedTab === index}
+            className={`p-4 w-full hover:bg-slate-300 ${selectedTab === index ? 'bg-slate-300' : ''}`} 
+            onClick={() => setSelectedTab(index)}>
+            Tab {index + 1}
+          </button>
+        ))}
+      </div>
+        <div className='grid grid-cols-1 p-4 text-slate-100'>
+          <div className='col-[1/2] row-[1/2]'>
+            <p>{tabsData[selectedTab].txt}</p>
+          </div>
+        </div>
+    </div>
+  )
+}
